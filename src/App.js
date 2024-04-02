b53a64d4269ede560845f04e74c28b8bb6131c25
Ashish <dewandameena1098@gmail.com>
2024-04-02 17:06:41 +0530

    footer completed..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className='bg-[#0e1630] h-screen'>
       <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="container mx-auto px-8 max-w-screen-lg text-white grid grid-flow-col items-start ">
+        <div>
+          <div className="mb-12 text-[#01d293] text-3xl">Connect with me</div>
+          <div className="mb-3 text-[#809EAD] grid grid-flow-col items-center">
+            <i className="text-[#01d293] mr-4 text-1xl grid col-span-1 ">
+              <ion-icon name="location-outline"></ion-icon>
+            </i>
+            <span className="grid col-span-11">Jaipur, India</span>
+          </div>
+          <div className="mb-3 text-[#809EAD] grid grid-flow-col items-center">
+            <i className="text-[#01d293] mr-4 text-1xl  ">
+              <ion-icon name="mail-outline"></ion-icon>
+            </i>
+            <span className="grid col-span-11 hover:text-[#01d293] cursor-pointer transition duration-300">
+              dewandameena1098@gmail.com
+            </span>
+          </div>
+        </div>
+
+        <div className="">
+          <div className="mb-12 text-[#01d293] text-3xl">Contact me</div>
+          <div>
+            <form action="">
+              <div className="">
+                <input
+                  className="appearance-none outline-none bg-gray-700 w-full focus:bg-gray-700 text-gray-300 border-2 border-gray-400 focus:border-[#01d293] py-3 px-4 mb-3 rounded"
+                  type="text"
+                  placeholder="Your Full Name"
+                />
+              </div>
+              <div>
+                <input
+                  className="appearance-none outline-none bg-gray-700 w-full focus:bg-gray-700 text-gray-300 border-2 border-gray-400 focus:border-[#01d293] py-3 px-4 mb-3 rounded"
+                  type="text"
+                  placeholder="Your Email"
+                />
+              </div>
+              <div>
+                <textarea
+                  className="appearance-none outline-none bg-gray-700 w-full focus:bg-gray-700 text-gray-300 border-2 border-gray-400 focus:border-[#01d293] py-3 px-4 mb-3 rounded"
+                  rows="4"
+                  type="text"
+                  placeholder="Your Message"
+                />
+              </div>
+              <div className=" ">
+                <button className="bg-gray-700 hover:bg-[#01d293] transition ease-in-out duration-700 text-white py-2 px-4 rounded">
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
