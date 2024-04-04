3c09adc43acd72fdbdd80677d76e91d4bf5f4374
Ashish <dewandameena1098@gmail.com>
2024-04-04 16:37:13 +0530

    projects section template added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Home from './Home';
+import Projects from './Projects';
 
 function App() {
   return (
     <div className='bg-[#0e1630] w-screen overflow-x-hidden'>
       <Navbar />
       <Home />
-      <Footer/>
+      <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/Projects.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <>
+      <div className="container mx-auto max-w-screen-lg px-8 text-white">
+        <div className="mb-8 text-[#01d293] text-3xl">My Projects</div>
+        <div className=" grid md:grid-cols-3 gap-10 wrap ">
+          <div className="h-full w-full md:h-[450px] md:w-[300px] mb-10 grid col-span-1 grid-flow-row bg-[#1d284ba1] shadow-xl p-4 rounded-lg ">
+            <div>
+              <img
+                className="w-full h-full rounded-lg object-cover"
+                src="/images/myPhoto.jpg"
+                alt=""
+              />
+            </div>
+            <div className="mb-3 mt-3 text-3xl">Full Stack Twitter Clone</div>
+            <div className="grid grid-flow-col grid-rows-2 gap-2">
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Node Js
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Express
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Tailwind
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                MongoDB
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                MongoDB
+              </span>
+            </div>
+          </div>
+          <div className="h-full w-full md:h-[450px] md:w-[300px] mb-10 grid col-span-1 grid-flow-row bg-[#1d284ba1] shadow-xl p-4 rounded-lg ">
+            <div>
+              <img
+                className="w-full h-full rounded-lg object-cover"
+                src="/images/myPhoto.jpg"
+                alt=""
+              />
+            </div>
+            <div className="mb-3 mt-3 text-3xl">Full Stack Twitter Clone</div>
+            <div className="grid grid-flow-col grid-rows-2 gap-2">
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Node Js
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Express
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Tailwind
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                MongoDB
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                MongoDB
+              </span>
+            </div>
+          </div>
+          <div className="h-full w-full md:h-[450px] md:w-[300px] mb-10 grid col-span-1 grid-flow-row bg-[#1d284ba1] shadow-xl p-4 rounded-lg ">
+            <div>
+              <img
+                className="w-full h-full rounded-lg object-cover"
+                src="/images/myPhoto.jpg"
+                alt=""
+              />
+            </div>
+            <div className="mb-3 mt-3 text-3xl">Full Stack Twitter Clone</div>
+            <div className="grid grid-flow-col grid-rows-2 gap-2">
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Node Js
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Express
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                Tailwind
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                MongoDB
+              </span>
+              <span className="bg-gray-700 rounded flex justify-center items-center">
+                MongoDB
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
